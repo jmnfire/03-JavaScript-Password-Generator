8d8630333d9c2b57356549680c15bdac31915907
Jovan Nixon <jmnfire@gmail.com>
2021-03-04 11:10:25 -0700

    Final1

--- README.md
+++ README.md
@@ -5,8 +5,58 @@
 
 A password is randomly generated based on criteria that the user provides when prompted.
 
-When the user clicks the button "Generate Password" they are first promoted for password length. If outside the range of 8-50, the user is prompted to once again enter a password length within the range. The prompt will loop until the user enters a valid number.
+When the user clicks the button "Generate Password" they are first promoted for password length. If outside the range of 8-50, the user will have to keep putting  numbers until its inside the length within the range. The prompt will loop until the user enters a valid number.
 
-The program then prompts the user to define if they would like to include numbers, characters, uppercase and/or lowercase letters. If the user selects none to all criteria, the program will ask it to select at least each one and will loop back through the prompts.
+The program then prompts the user to define if they would like to include numbers, characters, uppercase or lowercase letters. If the user selects none to all criteria, the program will ask it to select at least each one and will loop back through the prompts. If the user does not pick at least one of the criteria, a prompt will let the use know they need to regenerate a new password. 
 
-When all parameters have been defined, the password will appear within the password box
+When all parameters have been defined, the password will appear within the password box.
+
+## Table of Contents
+* [Features](#Features)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Badges](#Badges)
+
+## Features
+- for loop
+- while
+- if 
+- else
+- return
+- break
+
+## Installation 
+
+NO installation required you can view the page. If you want to look at my code, do a git clone or my repository. 
+
+[Link to website]()
+
+## Usage
+
+![screenshot](images/screencapture-file-Users-JovanNixon-Desktop-Homework-03-JavaScript-Password-Generator-index-html-2021-03-04-11_04_47.png)
+
+
+## License 
+
+MIT License
+Copyright (c) 2021 Jovan Nixon
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+## Badges
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
--- script.js
+++ script.js
@@ -1,83 +1,84 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// function generatePassword() {
+
+var specialChar = '!@#$%&(*)';
+var numberChar = '1234567890';
+var upperChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowerChar = 'abcdefghijklmnopqrstuvwxyz';
+
+var upper, lower, number, special = false;
+
 function generatePassword() {
-
-  var specialChar = '!@#$%&(*)';
-
-  var numbersChar = '1234567890';
-
-  var upperChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-  var lowerChar = 'abcdefghijklmnopqrstuvwxyz';
-
-  var passwordLength = 0;
-
-  var upper, lower, numbers, special = false;
-
-  var password = ''
-
-
-  
-  while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
-    passwordLength = prompt("How many characters would you like your password? Choose between 8 and 128");
+  var confirmLength = '';
+  var password = "";
+  //library for all the characters selected
+  var userChoices = "";
+  //Primitive types: strings, numbers, booleans (0 false, 1 true)
+  while (isNaN(confirmLength) || confirmLength < 8 || confirmLength > 128) {
+    confirmLength = prompt("How many characters would you like your password? Choose between 8 and 128"); //always give back a string
+    confirmLength = parseInt(confirmLength); //converts a string to an integer
+    if (confirmLength === true) {
+      break;
+    }
   }
 
-  while (passwordLength < 8 || passwordLength > 128) {
-    password = prompt("You must choose between 8 and 128");
-    console.log("Password length " + passwordLength);
+  if (confirmLength) {
+    if (confirm("Would you like to use lowercase characters?") == true) {
+      lower = true
+      userChoices = lowerChar + userChoices; //userChoices.concat(lowerChar)
+      console.log(true)
+    }
+
+    if (confirm("Would you like to use uppercase characters?") == true) {
+      upper = true
+      userChoices = userChoices + upperChar;
+      console.log(true)
+    }
+
+    if (confirm("Would you like to use special characters?") == true) {
+      special = true
+      userChoices = userChoices + specialChar;
+      console.log(true)
+    }
+
+    if (confirm("Would you like to use numerical characters?") == true) {
+      number = true
+      userChoices = userChoices + numberChar;
+      console.log(true)
+    }
+    //if none of the character types are selected, alerts the user to choose at least one
+    if (!lower && !upper && !special && !number) {
+      alert("At least one character type must be selected")
+
+    } else {
+      for (var i = 0; i < confirmLength; i++) {
+        var singleRandomChar = userChoices[Math.floor(Math.random() * userChoices.length)];
+        password += singleRandomChar; //+= means add the variable and a new variable together
+        console.log(singleRandomChar);
+      }
+    }
+
+    return password;
   }
 
-  while (!upper || !lower || !numbers || !special) {
-    upper = confirm("Will this contain upper case letters?");
-    lower = confirm("Will this contain lower case letters?");
-    numbers = confirm("Will this contain numbers?");
-    special = confirm("Will this contain special characters?");
-  }
 }
-
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-    // Loop for random selection
-    for (var i = 0; i < password; i++) {
-      var allChoices = userChoices[Math.floor(Math.random() * userChoices.length)];
-      passwordBlank.push(allChoices);
-      console.log(allChoices);
-    }
+  if (!password) {
+    passwordText.value = "You must regenerate a password ";
+  } else {
+    passwordText.value = password; //textareas and forms have a special attribute called value which allows text modification
   }
 
-    // var password = passwordBlank.join("");
-    // console.log("Your Pasword is: " + password);
-    // return password;
+}
 
 
- // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-
-  //   characters = alert("Here is your new password")
-  //   console.log("Password")
-
-  //   if (generatePassword) {
-  //     charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#$%&!?/0123456789",
-  //       retVal = "";
-  //     for (var i = 0, n = charset.length; i < length; ++i) {
-  //       retVal += charset.charAt(Math.floor(Math.random() * n));
-  //     }
-  //     return retVal;
-  //   }
-
-  //   var password = passwordBlank.join("");
-  //   console.log("Your Password is: " + password);
-  //   return password;
-
-  // }
-
-  console.log(generatePassword(password));
+//console.log(generatePassword());
